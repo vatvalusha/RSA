bc50894ebbaa97b527015910da0a14bec56d2530
vatvalusha <vatvalusha@gmail.com>
2017-02-10 10:24:20 +0200

    Fix classes

---
+++ src/main/java/prime_number/GeneratePrimeNumber.java
@@ -0,0 +1,7 @@
+package prime_number;
+
+/**
+ * Created by vatva on 10.02.2017.
+ */
+public class GeneratePrimeNumber {
+}
---
+++ src/test/java/abonent/AbonentTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by vatva on 09.02.2017.
+ */
+public class AbonentTest {
+
+}
